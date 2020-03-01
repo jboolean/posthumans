8aeb79136e0be851b9ffbabae651cc2a509c8914
Julian Boilen <julianboilen@gmail.com>
2020-03-01 15:36:26 -0500

    Use https

--- _data/site.js
+++ _data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   "title": "Posthumans",
   "publicPath": process.env.URL || 'http://localhost:8080',
-  "filesPath": process.env.FILES_URL || 'http://d20iws37zrcaba.cloudfront.net/files'
+  "filesPath": process.env.FILES_URL || 'https://d20iws37zrcaba.cloudfront.net/files'
 };
